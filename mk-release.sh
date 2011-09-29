0e33453b8abdd00b8142c83d797db45909303c32
Yang Liu <yang.liu.mail@gmail.com>
2011-09-30 02:22:49 +0800

    e

--- mk-release.sh
+++ mk-release.sh
@@ -9,13 +9,13 @@ TD=`mktemp -d /tmp/XXXXXXXXXXXXX`
 
 ( cd $TD 
   svn export $URL
-  mv wessels dnscap-$REV
-  rm dnscap-$REV/mk-release.sh
-  tar czvf /tmp/dnscap-$REV.tar.gz dnscap-$REV
+  mv wessels dnspy-$REV
+  rm dnspy-$REV/mk-release.sh
+  tar czvf /tmp/dnspy-$REV.tar.gz dnspy-$REV
 )
 
 rm -rfv $TD
-echo "install -m 644 -o $USER /tmp/dnscap-$REV.tar.gz /usr/local/www/dnscap"
-install -m 644 -o $USER /tmp/dnscap-$REV.tar.gz /usr/local/www/dnscap
-(cd /usr/local/www/dnscap/; md5 *.tar.gz > MD5s.txt)
+echo "install -m 644 -o $USER /tmp/dnspy-$REV.tar.gz /usr/local/www/dnspy"
+install -m 644 -o $USER /tmp/dnspy-$REV.tar.gz /usr/local/www/dnspy
+(cd /usr/local/www/dnspy/; md5 *.tar.gz > MD5s.txt)
 
